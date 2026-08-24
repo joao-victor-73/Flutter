34f96182563250f511c28f18373bac4e6cb581b6
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2026-08-24 18:52:08 -0300

    tentando voltar com o projeto

---
+++ projeto_gerenciamento/api/models/usuario_models.py
@@ -0,0 +1,14 @@
+from api import db
+from passlib.hash import pbkdf2_sha256
+
+
+class Usuario(db.Model):
+    __tablename__ = 'usuario'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    nome = db.Column(db.String(45), nullable=False)
+    email = db.Column(db.String(90), nullable=False)
+    senha = db.Column(db.String(265), nullable=False)
+
+    def  cripto_senha(self):
+        self.senha = pbkdf2_sha256(self.senha)
